982b2ec312cb7da4198abc7cb6773e05b2c0492b
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-26 17:51:38 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/x86_64-w64-mingw64.cmake
+++ cmake/x86_64-w64-mingw64.cmake
@@ -3,12 +3,13 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_BUILD_TYPE  Debug)
 set(TOOLCHAIN_PREFIX  x86_64-w64-mingw32)
 
-# cross compilers to use for C and C++
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}-as)
-set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_RC_COMPILER  ${TOOLCHAIN_PREFIX}-windres)
-
 # target environment on the build host system
 #   set 1st to dir with the cross compiler's C/C++ headers/libs
 set(CMAKE_FIND_ROOT_PATH C:/msys64/mingw64)
+set(CMAKE_MAKE_PROGRAM ${CMAKE_FIND_ROOT_PATH}/bin/mingw32-makez)
+
+# cross compilers to use for C and C++
+set(CMAKE_ASM_COMPILER ${CMAKE_FIND_ROOT_PATH}/bin/${TOOLCHAIN_PREFIX}-as)
+set(CMAKE_C_COMPILER   ${CMAKE_FIND_ROOT_PATH}/bin/${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${CMAKE_FIND_ROOT_PATH}/bin/${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_RC_COMPILER  ${CMAKE_FIND_ROOT_PATH}/bin/${TOOLCHAIN_PREFIX}-windres)
